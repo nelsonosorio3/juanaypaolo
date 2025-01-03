9d652117d6cf3fc0635418623902e651befc7f99
neel <nelson.osorio@hotmail.com>
2025-01-03 15:06:20 -0500

    added recommendations

--- src/components/RecommendationsSection.jsx
+++ src/components/RecommendationsSection.jsx
@@ -7,16 +7,30 @@ function RecommendationsSection({ language }) {
     <section>
       <h2>{texts[language].recommendationsTitle}</h2>
       <div className="rec-section">
-        <h4>{texts[language].hotels}</h4>
-        <p>{texts[language].recStay}</p>
+        <h4>{texts[language].arrival}</h4>
+        <p>{texts[language].recArrival}</p>
       </div>
       <div className="rec-section">
-        <h4>{texts[language].activities}</h4>
-        <p>{texts[language].recFood}</p>
+        <h4>{texts[language].hotels}</h4>
+        <p>{texts[language].recStay1}</p>
+        <p><a href={"https://search.app/YJ5cumRcGso4GzYP8"} target={'_blank'} rel="noreferrer">{texts[language].recStay2}</a>{texts[language].recStay3}</p>
+        <p>{texts[language].recStay4}</p>
+        <p>{texts[language].recStay5}</p>
+        <p>{texts[language].recStay6}</p>
       </div>
       <div className="rec-section">
         <h4>{texts[language].transportation}</h4>
-        <p>{texts[language].recTrans}</p>
+        <p>{texts[language].recTransportation1}</p>
+        <p>{texts[language].recTransportation2}</p>
+      </div>
+      <div className="rec-section">
+        <h4>{texts[language].nightlife}</h4>
+        <p>{texts[language].recNightlife1}<b>{texts[language].recNightlife2}</b>{texts[language].recNightlife3}</p>
+      </div>
+      <div className="rec-section">
+        <h4>{texts[language].activities}</h4>
+        <p>{texts[language].recActivities1}</p>
+        <p>{texts[language].recActivities2}</p>
       </div>
     </section>
   );
